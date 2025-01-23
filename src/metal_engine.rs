9f39bef5eea45c1fd1979d166e6feaebe7612898
Rumble Fish <rumblefish@192.168.1.24>
2025-01-23 12:53:51 +0100

    cleanup
    Signed-off-by: Rumble Fish <rumblefish@192.168.1.24>
    Signed-off-by: Misieq01 <bartosz2000walczak@gmail.com>

--- src/metal_engine.rs
+++ src/metal_engine.rs
@@ -1,8 +1,18 @@
-use std::mem::{size_of,transmute};
+use std::mem::{size_of, transmute};
 
-use log::{debug, info};
+use log::{debug, error};
 use metal::{
-    objc::rc::autoreleasepool, ArgumentDescriptor, Array, CompileOptions, ComputePassDescriptor, ComputePipelineDescriptor, Device, Fence, FenceRef, Function, Library, MTLResourceOptions, MTLResourceUsage, MTLSize, NSUInteger, Texture
+    objc::rc::autoreleasepool,
+    Buffer,
+    CompileOptions,
+    ComputePipelineDescriptor,
+    ComputePipelineState,
+    Device,
+    Function,
+    Library,
+    MTLResourceOptions,
+    MTLResourceUsage,
+    MTLSize,
 };
 
 use crate::{
@@ -37,21 +47,17 @@ impl FunctionImpl for MetalFunction {
         });
 
         let block_size = kernel.device().recommended_max_working_set_size() as u32;
-        let grid_size = kernel.device().max_threadgroup_memory_length() as u32;
+        let grid_size = kernel.device().max_threads_per_threadgroup().width as u32;
 
         Ok((block_size, grid_size))
     }
 }
 
 #[derive(Clone)]
-pub struct MetalEngine {
-    sum: u32,
-}
+pub struct MetalEngine {}
 impl MetalEngine {
     pub fn new() -> Self {
-        MetalEngine {
-            sum: 0,
-        }
+        MetalEngine {}
     }
 }
 
@@ -85,15 +91,15 @@ impl EngineImpl for MetalEngine {
             };
 
             if let Ok(context) = self.create_context(gpu_device.device_index).inspect_err(|error| {
-                debug!(target: LOG_TARGET,"Failed to create context: {:?}", error);
+                error!(target: LOG_TARGET,"Failed to create context: {:?}", error);
             }) {
                 if let Ok(function) = self.create_main_function(&context).inspect_err(|error| {
-                    debug!(target: LOG_TARGET,"Failed to create main function: {:?}", error);
+                    error!(target: LOG_TARGET,"Failed to create main function: {:?}", error);
                 }) {
                     if let Ok((block_size, grid_size)) = function
                         .suggested_launch_configuration(&context.context)
                         .inspect_err(|error| {
-                            debug!(target: LOG_TARGET,"Failed to get suggested launch configuration: {:?}", error);
+                            error!(target: LOG_TARGET,"Failed to get suggested launch configuration: {:?}", error);
                         })
                     {
                         gpu_device.block_size = block_size;
@@ -114,17 +120,15 @@ impl EngineImpl for MetalEngine {
 
     fn create_context(&self, device_index: u32) -> Result<Self::Context, anyhow::Error> {
         let all_devices = Device::all();
-        let device = all_devices.get(device_index as usize).unwrap_or_else(|| {
-            panic!("Failed to get device with index: {}", device_index);
-        });
+        let device = all_devices
+            .get(device_index as usize)
+            .ok_or(anyhow::anyhow!("create_context: Device not found"))?;
 
         Ok(MetalContext::new(device.clone()))
     }
 
     fn create_main_function(&self, context: &Self::Context) -> Result<Self::Function, anyhow::Error> {
-        let function = create_program_from_source(&context.context).unwrap_or_else(|error| {
-            panic!("Failed to create program from source: {:?}", error);
-        });
+        let function = create_program_from_source(&context.context)?;
         Ok(MetalFunction { program: function })
     }
 
@@ -139,209 +143,153 @@ impl EngineImpl for MetalEngine {
         block_size: u32,
         grid_size: u32,
     ) -> Result<(Option<u64>, u32, u64), anyhow::Error> {
-        info!(target: LOG_TARGET,"MetalEngine: Mining");
-        info!(target: LOG_TARGET,"Data: {:?}", data);
-        info!(target: LOG_TARGET,"Min difficulty: {}", min_difficulty);
-        info!(target: LOG_TARGET,"Nonce start: {}", nonce_start);
-        info!(target: LOG_TARGET,"Num iterations: {}", num_iterations);
+        autoreleasepool(|| {
+            let command_queue = context.context.new_command_queue();
 
-      autoreleasepool(|| {
-        let command_queue = context.context.new_command_queue();
+            let (buffer, min_difficulty_buffer, nonce_start_buffer, num_iterations_buffer, output) =
+                create_buffers(&context.context, data, min_difficulty, nonce_start, num_iterations);
 
-        let buffer = context.context.new_buffer_with_data(
-            unsafe { transmute(data.as_ptr()) },
-            (data.len() * size_of::<u32>()) as u64,
-            MTLResourceOptions::CPUCacheModeDefaultCache,
-        );
+            let command_buffer = command_queue.new_command_buffer();
+            let encoder = command_buffer.new_compute_command_encoder();
 
-        info!(target: LOG_TARGET,"Creating min_difficulty buffer");
+            let kernel = function
+                .program
+                .get_function("sha3", None)
+                .map_err(|error| anyhow::anyhow!("Failed to get function sha3: {:?}", error))?;
 
-        let min_difficulty_buffer = {
-            let min_difficulty_data = [min_difficulty];
-            context.context.new_buffer_with_data(
-                unsafe { transmute(min_difficulty_data.as_ptr()) },
-                (min_difficulty_data.len() * size_of::<u64>()) as u64,
-                MTLResourceOptions::CPUCacheModeDefaultCache,
-            )
-        };
+            let pipeline_state = create_pipeline_state(&context.context, &kernel)?;
+            encoder.set_compute_pipeline_state(&pipeline_state);
 
-        info!(target: LOG_TARGET,"Creating nonce start buffer");
+            debug!(target: LOG_TARGET,"Setting buffers for arguments");
+            encoder.set_buffer(0, Some(&buffer), 0);
+            encoder.set_buffer(1, Some(&output), 0);
+            encoder.set_buffer(2, Some(&nonce_start_buffer), 0);
+            encoder.set_buffer(3, Some(&min_difficulty_buffer), 0);
+            encoder.set_buffer(4, Some(&num_iterations_buffer), 0);
 
-        let nonce_start_buffer = {
-            let nonce_start_data = [nonce_start];
-            context.context.new_buffer_with_data(
-                unsafe { transmute(nonce_start_data.as_ptr()) },
-                (nonce_start_data.len() * size_of::<u64>()) as u64,
-                MTLResourceOptions::CPUCacheModeDefaultCache,
-            )
-        };
+            debug!(target: LOG_TARGET,"Describing resources");
+            encoder.use_resource(&buffer, MTLResourceUsage::Read);
+            encoder.use_resource(&min_difficulty_buffer, MTLResourceUsage::Read);
+            encoder.use_resource(&nonce_start_buffer, MTLResourceUsage::Read);
+            encoder.use_resource(&num_iterations_buffer, MTLResourceUsage::Read);
+            encoder.use_resource(&output, MTLResourceUsage::Write);
+            encoder.memory_barrier_with_resources(&[&output]);
 
-        info!(target: LOG_TARGET,"Creating num_iterations buffer");
+            let threads = (block_size / 2).min(grid_size / 2) as u64;
 
-        let num_iterations_buffer = {
-            let num_iterations_data = [num_iterations];
-            context.context.new_buffer_with_data(
-                unsafe { transmute(num_iterations_data.as_ptr()) },
-                (num_iterations_data.len() * size_of::<u32>()) as u64,
-                MTLResourceOptions::CPUCacheModeDefaultCache,
-            )
-        };
+            let threads_per_thread_group = MTLSize {
+                width: threads,
+                height: 1,
+                depth: 1,
+            };
 
-        info!(target: LOG_TARGET,"Creating sum buffer");
+            let threads_per_grid = MTLSize {
+                width: (data.len() as f64 / threads_per_thread_group.width as f64).ceil() as u64,
+                height: 1,
+                depth: 1,
+            };
 
-        let output = {
-            let output_data =  vec![0u64, 0u64];
-            context.context.new_buffer_with_data(
-                unsafe { transmute(output_data.as_ptr()) },
-                (output_data.len() * size_of::<u64>()) as u64,
-                MTLResourceOptions::CPUCacheModeDefaultCache,
-            )
-        };
+            debug!(target: LOG_TARGET,"Threads per thread group: {:?}", threads_per_thread_group);
+            debug!(target: LOG_TARGET,"Threads per grid: {:?}", threads_per_grid);
 
+            encoder.dispatch_threads(threads_per_grid, threads_per_thread_group);
+            encoder.end_encoding();
 
-        // let min_difficulty_argument_descriptor = ArgumentDescriptor::new();
-        // min_difficulty_argument_descriptor.set_index(0);
-        // min_difficulty_argument_descriptor.set_data_type(metal::MTLDataType::ULong);
+            command_buffer.commit();
+            command_buffer.wait_until_completed();
 
-        // let nonce_start_argument_descriptor = ArgumentDescriptor::new();
-        // nonce_start_argument_descriptor.set_index(1);
-        // nonce_start_argument_descriptor.set_data_type(metal::MTLDataType::ULong);
-
-        // let num_iterations_argument_descriptor = ArgumentDescriptor::new();
-        // num_iterations_argument_descriptor.set_index(2);
-        // num_iterations_argument_descriptor.set_data_type(metal::MTLDataType::UInt);
-
-        let command_buffer = command_queue.new_command_buffer();
-        let encoder = command_buffer.new_compute_command_encoder();
-
-        let kernel = function.program.get_function("sha3",None).unwrap();
-
-        info!(target: LOG_TARGET,"Creating argument encoder: {:?}", context.context.argument_buffers_support());
-
-        // let argument_encoder = context.context.new_argument_encoder(Array::from_slice(
-        //     &[
-        //         nonce_start_argument_descriptor,
-        //         min_difficulty_argument_descriptor,
-        //         num_iterations_argument_descriptor,
-        //     ],
-        // ));
-        // let argument_encoder = kernel.new_argument_encoder(2);
-
-        // info!(target: LOG_TARGET,"Creating argument buffer: {:?}", argument_encoder.encoded_length());
-
-        // let arg_buffer = context.context.new_buffer(
-        //     argument_encoder.encoded_length(),
-        //     MTLResourceOptions::empty(),
-        // );
-
-        // info!(target: LOG_TARGET,"Setting argument buffer");
-
-        // argument_encoder.set_argument_buffer(&arg_buffer, 0);
-        // info!(target: LOG_TARGET,"Argument buffer size: {:?}", arg_buffer.allocated_size());
-        // info!(target: LOG_TARGET,"Setting buffer nonce_start");
-        // argument_encoder.set_buffer(0, &nonce_start, 0);
-        // info!(target: LOG_TARGET,"Argument buffer size: {:?}", arg_buffer.allocated_size());
-        // info!(target: LOG_TARGET,"Setting buffer min_difficulty");
-        // argument_encoder.set_buffer(1, &min_difficulty,0);
-        // info!(target: LOG_TARGET,"Argument buffer size: {:?}", arg_buffer.allocated_size());
-        // info!(target: LOG_TARGET,"Setting buffer num_iterations");
-        // argument_encoder.set_buffer(2, &num_iterations, 0);
-
-        info!(target: LOG_TARGET,"Setting pipeline state descriptor");
-        let pipeline_state_descriptor = ComputePipelineDescriptor::new();
-        pipeline_state_descriptor.set_compute_function(Some(&kernel));
-
-        info!(target: LOG_TARGET,"Creating pipeline state");
-
-        let pipeline_function = pipeline_state_descriptor.compute_function().unwrap();
-
-        info!(target: LOG_TARGET,"Creating compute pipeline state: {:?}", pipeline_function.name());
-
-        let pipeline_state = context.context
-            .new_compute_pipeline_state_with_function(
-                pipeline_state_descriptor.compute_function().unwrap(),
-            )
-            .unwrap();
-
-        info!(target: LOG_TARGET,"Setting compute pipeline state");
-
-        encoder.set_compute_pipeline_state(&pipeline_state);
-        encoder.set_buffer(0, Some(&buffer), 0);
-        encoder.set_buffer(1, Some(&output), 0);
-        encoder.set_buffer(2, Some(&nonce_start_buffer), 0);
-        encoder.set_buffer(3, Some(&min_difficulty_buffer), 0);
-        encoder.set_buffer(4, Some(&num_iterations_buffer), 0);
-        // encoder.set_buffer(2, Some(&arg_buffer), 0);
-
-        info!(target: LOG_TARGET,"Using resources");
-
-        encoder.use_resource(&buffer, MTLResourceUsage::Read);
-        encoder.use_resource(&min_difficulty_buffer, MTLResourceUsage::Read);
-        encoder.use_resource(&nonce_start_buffer, MTLResourceUsage::Read);
-        encoder.use_resource(&num_iterations_buffer, MTLResourceUsage::Read);
-        encoder.use_resource(&output, MTLResourceUsage::Write);
-        // encoder.memory_barrier_with_resources(&[&output]);
-
-        let threads_per_thread_group = MTLSize {
-            width: 24,
-            height: 1,
-            depth: 1,
-        };
-
-        let threads_per_grid = MTLSize {
-            width: (data.len() as f64 / threads_per_thread_group.width as f64).ceil() as u64,
-            height: 1,
-            depth: 1,
-        };
-
-        info!(target: LOG_TARGET,"Threads per thread group: {:?}", threads_per_thread_group);
-        info!(target: LOG_TARGET,"Threads per grid: {:?}", threads_per_grid);
-
-        encoder.dispatch_threads(threads_per_grid, threads_per_thread_group);
-            
-
-        // let width = 16;
-
-        // let thread_group_count = MTLSize {
-        //     width,
-        //     height: 1,
-        //     depth: 1,
-        // };
-
-        // let thread_group_size = MTLSize {
-        //     width: (data.len() as u64 + width) / width,
-        //     height: 1,
-        //     depth: 1,
-        // };
-
-        // info!(target: LOG_TARGET,"Thread group size: {:?}", thread_group_size);
-        // info!(target: LOG_TARGET,"Thread group count: {:?}", thread_group_count);
-
-        // encoder.dispatch_thread_groups(thread_group_count, thread_group_size);
-        
-        encoder.end_encoding();
-        info!(target: LOG_TARGET,"Command buffer commit");
-        command_buffer.commit();
-        command_buffer.wait_until_completed();
-
-
-
-        let ptr = output.contents() as *mut [u64; 2];
-        unsafe {
-            let result = *ptr;
-            info!(target: LOG_TARGET,"Result: {:?}", result);
-            if result[0] > 0 {
-                return Ok((Some(
-                    result[0]),
-                    grid_size * block_size * num_iterations,
-                    u64::MAX / result[1]));
-            }else {
-                return Ok((None, grid_size * block_size * num_iterations, u64::MAX / result[1]));
+            let ptr = output.contents() as *mut [u64; 2];
+            unsafe {
+                let result = *ptr;
+                if result[0] > 0 {
+                    return Ok((Some(result[0]), (threads as u32) * num_iterations, u64::MAX / result[1]));
+                } else {
+                    return Ok((None, (threads as u32) * num_iterations, u64::MAX / result[1]));
+                }
             }
-        }
-    })
-
+        })
     }
+}
+
+fn create_buffers(
+    context: &Device,
+    data: &[u64],
+    min_difficulty: u64,
+    nonce_start: u64,
+    num_iterations: u32,
+) -> (Buffer, Buffer, Buffer, Buffer, Buffer) {
+    debug!(target: LOG_TARGET,"MetalEngine: Creating buffers");
+
+    debug!(target: LOG_TARGET,"Creating data buffer with data: {:?}", data);
+    let data_buffer = context.new_buffer_with_data(
+        unsafe { transmute(data.as_ptr()) },
+        (data.len() * size_of::<u32>()) as u64,
+        MTLResourceOptions::CPUCacheModeDefaultCache,
+    );
+
+    debug!(target: LOG_TARGET,"Creating min_difficulty buffer with data: {:?}", min_difficulty);
+    let min_difficulty_buffer = {
+        let min_difficulty_data = [min_difficulty];
+        context.new_buffer_with_data(
+            unsafe { transmute(min_difficulty_data.as_ptr()) },
+            (min_difficulty_data.len() * size_of::<u64>()) as u64,
+            MTLResourceOptions::CPUCacheModeDefaultCache,
+        )
+    };
+
+    debug!(target: LOG_TARGET,"Creating nonce_start buffer with data: {:?}", nonce_start);
+    let nonce_start_buffer = {
+        let nonce_start_data = [nonce_start];
+        context.new_buffer_with_data(
+            unsafe { transmute(nonce_start_data.as_ptr()) },
+            (nonce_start_data.len() * size_of::<u64>()) as u64,
+            MTLResourceOptions::CPUCacheModeDefaultCache,
+        )
+    };
+
+    debug!(target: LOG_TARGET,"Creating num_iterations buffer with data: {:?}", num_iterations);
+    let num_iterations_buffer = {
+        let num_iterations_data = [num_iterations];
+        context.new_buffer_with_data(
+            unsafe { transmute(num_iterations_data.as_ptr()) },
+            (num_iterations_data.len() * size_of::<u32>()) as u64,
+            MTLResourceOptions::CPUCacheModeDefaultCache,
+        )
+    };
+
+    debug!(target: LOG_TARGET,"Creating output buffer");
+    let output = {
+        let output_data = vec![0u64, 0u64];
+        context.new_buffer_with_data(
+            unsafe { transmute(output_data.as_ptr()) },
+            (output_data.len() * size_of::<u64>()) as u64,
+            MTLResourceOptions::CPUCacheModeDefaultCache,
+        )
+    };
+
+    (
+        data_buffer,
+        min_difficulty_buffer,
+        nonce_start_buffer,
+        num_iterations_buffer,
+        output,
+    )
+}
+
+fn create_pipeline_state(device: &Device, kernel: &Function) -> Result<ComputePipelineState, anyhow::Error> {
+    debug!(target: LOG_TARGET,"MetalEngine: Creating pipeline state");
+    let pipeline_state_descriptor = ComputePipelineDescriptor::new();
+    pipeline_state_descriptor.set_compute_function(Some(&kernel));
+
+    let compute_function = pipeline_state_descriptor
+        .compute_function()
+        .ok_or_else(|| anyhow::anyhow!("Failed to get compute function from pipeline state descriptor"))?;
+
+    let pipeline_state = device
+        .new_compute_pipeline_state_with_function(compute_function)
+        .map_err(|error| anyhow::anyhow!("Failed to create compute pipeline state: {:?}", error))?;
+
+    Ok(pipeline_state)
 }
 
 fn create_program_from_source(context: &Device) -> Result<Library, anyhow::Error> {
--- src/metal_sha3.metal
+++ src/metal_sha3.metal
@@ -1,6 +1,5 @@
 #include <metal_stdlib>
 #include <metal_atomic>
-#include <metal_compute>
 
 using namespace metal;
 
@@ -23,24 +22,13 @@ constant static const ulong RC[] = {
     0x8000000000008080ul, 0x0000000080000001ul, 0x8000000080008008ul,
 };
 
-ulong swap_endian_64(ulong value) {
-  return ((value & 0x00000000000000FFULL) << 56) |
-         ((value & 0x000000000000FF00ULL) << 40) |
-         ((value & 0x0000000000FF0000ULL) << 24) |
-         ((value & 0x00000000FF000000ULL) << 8) |
-         ((value & 0x000000FF00000000ULL) >> 8) |
-         ((value & 0x0000FF0000000000ULL) >> 24) |
-         ((value & 0x00FF000000000000ULL) >> 40) |
-         ((value & 0xFF00000000000000ULL) >> 56);
-}
-
 kernel void sha3(device ulong *buffer [[ buffer(0) ]],
                  device ulong *output_1 [[ buffer(1) ]],
                  device ulong& nonce_start [[ buffer(2) ]],
                  device ulong& difficulty [[ buffer(3) ]],
                  device uint& num_rounds [[ buffer(4) ]],
                  uint gid [[ thread_position_in_grid ]],
-                 uint max_total_threads_per_threadgroup [[ threads_per_threadgroup ]]
+                 uint threads_per_grid [[ threads_per_grid ]]
                  ) {
 
     ulong state[25];
@@ -48,7 +36,7 @@ kernel void sha3(device ulong *buffer [[ buffer(0) ]],
         for (uint j = 0; j < 25; j++) {
             state[j] = 0;
         }
-        state[0] = nonce_start + gid + i * max_total_threads_per_threadgroup;
+        state[0] = nonce_start + gid + i * threads_per_grid;
         state[1] = buffer[1];
         state[2] = buffer[2];
         state[3] = buffer[3];
@@ -156,12 +144,12 @@ kernel void sha3(device ulong *buffer [[ buffer(0) ]],
         }
 
         // Check difficulty
-        ulong swap = swap_endian_64(state[0]);
+        ulong swap = reverse_bits(state[0]);
         __atomic_thread_fence(memory_order::memory_order_relaxed);
         threadgroup_barrier(mem_flags::mem_threadgroup);
         if (swap < difficulty) {
             if (output_1[1] == 0 || output_1[1] > swap) {
-                output_1[0] = nonce_start + gid + i * max_total_threads_per_threadgroup;
+                output_1[0] = nonce_start + gid + i * threads_per_grid;
                 output_1[1] = swap;
             }
         } else {
